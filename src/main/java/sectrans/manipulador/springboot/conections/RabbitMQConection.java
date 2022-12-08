3ef3a55ced094a78504870dd93ed647b51924a4d
Douglas Marins <douglasmarinssouza@gmail.com>
2022-12-08 16:52:45 -0300

    Adição de criação de filas

---
+++ src/main/java/sectrans/manipulador/springboot/conections/RabbitMQConection.java
@@ -0,0 +1,52 @@
+package sectrans.manipulador.springboot.conections;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class RabbitMQConection {
+    private static final String NAME_EXCHANGE = "amq.direct";
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMQConection(AmqpAdmin amqpAdmin){
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    private Queue fila(String nameFila){
+        return new Queue(nameFila, true, false, false);
+    }
+
+    private DirectExchange chanceDirect(){
+        return new DirectExchange(NAME_EXCHANGE);
+    }
+
+    private Binding relation(Queue fila, DirectExchange swap){
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, chanceDirect().getName(), fila.getName(), null);
+    }
+
+    @PostConstruct
+    private void add(){
+        Queue filaProcess = this.fila(RabbitmqConstantes.FILA_PROCESS);
+        Queue filaErase = this.fila(RabbitmqConstantes.FILA_ERASE);
+
+        DirectExchange exchange = this.chanceDirect();
+
+        Binding linkProcess = this.relation(filaProcess, exchange);
+        Binding linkErase = this.relation(filaErase, exchange);
+
+        //Criando as filas RabbitMQ
+        this.amqpAdmin.declareQueue(filaProcess);
+        this.amqpAdmin.declareQueue(filaErase);
+
+        this.amqpAdmin.declareExchange(exchange);
+
+        this.amqpAdmin.declareBinding(linkProcess);
+        this.amqpAdmin.declareBinding(linkErase);
+    }
+
+}
---
+++ src/main/java/sectrans/manipulador/springboot/conections/RabbitmqConstantes.java
@@ -0,0 +1,6 @@
+package sectrans.manipulador.springboot.conections;
+
+public class RabbitmqConstantes {
+    public static final String FILA_PROCESS   = "PROCESS";
+    public static final String FILA_ERASE = "ERASE";
+}
